e1dce528eb04ce89ec049e21c525955b6c8c637e
Kieran Brahney <kieran@supportpal.com>
2021-06-21 21:05:23 +0100

    update semantic-release

---
+++ release.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  debug: true,
+  branch: 'master',
+  verifyConditions: [
+    '@semantic-release/changelog',
+    '@semantic-release/npm',
+    '@semantic-release/github',
+  ],
+  prepare: [
+    '@semantic-release/changelog',
+    '@semantic-release/npm',
+    '@semantic-release/git',
+  ],
+  publish: [
+    '@semantic-release/npm',
+    ['@semantic-release/github', {
+      'assets': [
+        { 'path': 'dist/pollcast.js' },
+        { 'path': 'dist/pollcast.min.js' },
+      ]
+    }],
+  ],
+  success: ['@semantic-release/github'],
+}
