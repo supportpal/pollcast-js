1e8c47305bd0f5930755c0bd84e27dd2df2c334c
Kieran Brahney <kieran@supportpal.com>
2021-06-22 12:56:36 +0100

    add pollcast base test

--- jest.config.js
+++ jest.config.js
@@ -2,5 +2,8 @@ module.exports = {
   testEnvironment: "jsdom",
   testMatch: [
     '<rootDir>/test/**/?(*.)(test).{js,jsx,ts,tsx}'
-  ]
+  ],
+  transformIgnorePatterns: [
+    "node_modules/(?!(laravel-echo)/)",
+  ],
 }
---
+++ test/pollcast.test.ts
@@ -0,0 +1,8 @@
+import PollcastConnector from '../src/pollcast'
+import { Connector } from '../src/broadcasting/connector'
+
+describe('Pollcast', () => {
+  it('to be instance of connector', () => {
+    expect(new PollcastConnector({})).toBeInstanceOf(Connector)
+  })
+})
