ec1b40837ee9f8d8010783e00930d8d7b1840139
bytestream <kieran@supportpal.com>
2026-02-17 10:36:25 +0000

    ci: use conventionalcommits in semantic-release

--- release.config.js
+++ release.config.js
@@ -1,24 +1,25 @@
 module.exports = {
   debug: true,
   branch: 'master',
-  verifyConditions: [
+  plugins: [
+    ['@semantic-release/commit-analyzer', {
+      preset: 'conventionalcommits'
+    }],
+    ['@semantic-release/release-notes-generator', {
+      preset: 'conventionalcommits'
+    }],
     '@semantic-release/changelog',
     '@semantic-release/npm',
-    '@semantic-release/github'
-  ],
-  prepare: [
-    '@semantic-release/changelog',
-    '@semantic-release/npm',
-    '@semantic-release/git'
-  ],
-  publish: [
-    '@semantic-release/npm',
+    {
+      path: '@semantic-release/git',
+      assets: ['CHANGELOG.md', 'package.json', 'package-lock.json'],
+      message: 'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}'
+    },
     ['@semantic-release/github', {
       assets: [
         { path: 'dist/pollcast.js' },
         { path: 'dist/pollcast.min.js' }
       ]
     }]
-  ],
-  success: ['@semantic-release/github']
+  ]
 }
